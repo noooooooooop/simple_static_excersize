4c669a19f709a4e7724b06772cb23aaf54b20d89
nooooooop <4056698+noooooooooop@users.noreply.github.com>
2024-05-19 02:14:57 -0400

    Rendering LeafNode and more tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,11 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
+
+if 'unittest.util' in __import__('sys').modules:
+    # Show full diff in self.assertEqual.
+    __import__('sys').modules['unittest.util']._MAX_LENGTH = 999999999
+
 
 class TestHTMLNode(unittest.TestCase):
     def test_eq_1(self):
@@ -36,6 +41,17 @@ class TestHTMLNode(unittest.TestCase):
 
     def test_invalid_1(self):
         self.assertRaises(ValueError, lambda: HTMLNode("test", "asdf", True))
+
+    def test_render_leafnode_1(self):
+        html = LeafNode("p", "This is a paragraph of text.").to_html()
+        self.assertEqual(html, "<p>This is a paragraph of text.</p>")
+
+    def test_render_leafnode_2(self):
+        html = LeafNode("a", "Click me!", {"href": "https://www.google.com"}).to_html()
+        self.assertEqual(html, "<a href=\"https://www.google.com\">Click me!</a>")
+
+    def test_render_leafnode_error_1(self):
+        self.assertRaises(ValueError, lambda: LeafNode("p"))
 
 
 if __name__ == "__main__":
