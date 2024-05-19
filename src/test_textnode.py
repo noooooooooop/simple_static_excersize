784091db79dc18f7e74195fe57d72b0c5957fe71
nooooooop <4056698+noooooooooop@users.noreply.github.com>
2024-05-19 00:22:54 -0400

    some more tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -24,5 +24,26 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("", "", "")
         self.assertEqual(node, node2)
 
+    def test_not_eq_1(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is another text node", "bold")
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_2(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "italic")
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_3(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold", "adsfasdfafsd")
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_4(self):
+        node = TextNode("This is a text node", "bold", "adsfasdfafsd")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertNotEqual(node, node2)
+
+
 if __name__ == "__main__":
     unittest.main()
