182fea265b1e1c9552716bbe8fa3b15d8e60642c
nooooooop <4056698+noooooooooop@users.noreply.github.com>
2024-05-19 00:15:39 -0400

    Adding test python class

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode
+
+def main():
+    test = TextNode('asdfasdfsfasdf', 'aasdfafsd', 'http://not.a.real.website.asdfasdfasdfasdfasdfadfdasf')
+    print(test)
+
+if __name__ == '__main__':
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,15 @@
+# from typing import Type
+
+class TextNode:
+    def __init__(self, text: str, text_type:str , url: str):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, value: object, /) -> bool:
+        if not isinstance(value, TextNode): return NotImplemented
+
+        return self.url == value.url and self.text_type == value.text_type and self.text == value.text
+
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
